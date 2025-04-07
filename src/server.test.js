deecd6e971c5b9d886345bf9749babfc23e1375b
Scripter7899 <134367846+Scripter7899@users.noreply.github.com>
2025-04-08 02:03:47 +0530

    Delete src/server.test.js

--- src/server.test.js
+++
@@ -1,8 +0,0 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-describe('Basic Test', function () {
-  it('should return true', function () {
-    expect(true).to.equal(true);
-  });
-});
