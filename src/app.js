3401a5b7e17ff8391ab3262729b9c3419f292d8f
Scripter7899 <134367846+Scripter7899@users.noreply.github.com>
2025-04-08 02:06:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Sample route
 app.get('/', (req, res) => {
   res.send('Hello from Azure DevOps!');
 });
